d6bdf9c9bf00739e84939c4fd54e47d0a34775d6
romain <romain.quentel.bzh29@gmail.com>
2024-06-22 16:57:26 +0200

    Add fake user-agent to bypass 429 Flickr API error

--- services/Flickr.ts
+++ services/Flickr.ts
@@ -129,6 +129,9 @@ class Flickr {
                     user_id: process.env.FLICKR_USER_ID,
                     format: 'json',
                     nojsoncallback: true
+                },
+                headers :{
+                    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0'
                 }
             })
             .then(response => response.data)
